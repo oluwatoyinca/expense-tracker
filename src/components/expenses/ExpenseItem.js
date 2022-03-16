dae5c4a4e5cde61de822cad67d81c254ec903ca9
Oluwatoyin Amushan <alexandradonalds@gmail.com>
2022-03-16 23:51:03 +0100

    Delete button css updated

--- src/components/expenses/ExpenseItem.js
+++ src/components/expenses/ExpenseItem.js
@@ -15,7 +15,7 @@ const ExpenseItem = (props) => {
                 <div className='expense-item__description'>
                     <h2>{props.title}</h2>
                     <div className='expense-item__price'>${props.amount}</div>
-                    <button className='expense-item__price' onClick={deleteExpense}>Delete</button>
+                    <button className='expense-item__del' onClick={deleteExpense}>Delete</button>
                 </div>
             </Card>
         </li>
